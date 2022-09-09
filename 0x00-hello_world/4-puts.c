4648cafcdc943c13d20624bb7f28b1d8ff1706a8
DivinePraise Eric <praisechinemerem@gmail.com>
2022-09-08 17:55:38 -0700

    bug fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,25 @@
-#!/bin/bash
-gcc -o "Programming is like building a multilingual puzzle" $CFILE
+#include <stdio.h>
+
+
+
+/**
+
+* main - C is functional
+
+*
+
+* Return: Always 0 (no error reports)
+
+*/
+
+int main(void)
+
+{
+    
+puts("\"Programming is like building a multilingual
+
+puzzle");
+
+return (0);
+
+}
