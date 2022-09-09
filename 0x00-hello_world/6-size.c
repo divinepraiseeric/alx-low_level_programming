bb8b61d0b09aff3e9e85a65d5f24e58f7635c258
DivinePraise Eric <praisechinemerem@gmail.com>
2022-09-08 18:25:58 -0700

    more bugs

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
 *
 * Return: Always 0. (No errors returned)
 */
-int main(void)  
+int main(void)
 {
 char a;
 int b;
@@ -13,7 +13,7 @@ long long int d;
 float e;
 printf("Size of a char: %d byte(s)\n", sizeof(a));
 printf("Size of an int: %d byte(s)\n", sizeof(b));
-printf("Size of a long int: %d byte(s)\n",sizeof(c));
+printf("Size of a long int: %d byte(s)\n", sizeof(c));
 printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 printf("Size of a float: %d byte(s)\n", sizeof(e));
 return (0);
