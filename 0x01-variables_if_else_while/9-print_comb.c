75991ab4c5060621a43146d989cd9e13738c7036
DivinePraise Eric <praisechinemerem@gmail.com>
2022-09-09 20:59:43 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Prints all possible combination of dingle digti numbers
+* Return: Always (...);
+*/
+int main(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+if (c != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
