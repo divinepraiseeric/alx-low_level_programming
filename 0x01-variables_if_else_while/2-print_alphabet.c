e7a1dbcb68d910858d6250c46d14dcc58650f750
DivinePraise Eric <praisechinemerem@gmail.com>
2022-09-09 20:08:04 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main -prints alphabets in lowercase
+*
+* Return: Always (high)
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
