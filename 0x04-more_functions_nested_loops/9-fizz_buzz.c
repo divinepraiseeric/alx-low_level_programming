b8647a7c4a638732c85ff4b9bb7e284fa4d6c0fe
DivinePraise Eric <praisechinemerem@gmail.com>
2022-09-15 21:55:15 -0700

    t 9 bug fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,33 +10,26 @@
 
 int main(void)
 {
-int num = 1;	
-while (num++ < 100)
+int num;
+
+for (num = 1; num <= 100; ++num)
 {
-if ((num % 3 == 0) && (num % 5 == 0))
-{
-printf("FizzBuzz ");
-}
-else if ((num % 3) == 0)
-{
-printf("Fizz ");
-}
-else if ((num % 5) == 0)
-{
-if (num != 100)
-{
-printf("Buzz ");
-}
-else
-{
+if (num % 3 == 0 && !(num % 5 == 0))
+printf("Fizz");
+else if (num % 5 == 0 && !(num % 3 == 0))
 printf("Buzz");
-}
-}
+else if (num % 3 == 0 && num % 5 == 0)
+printf("FizzBuzz");
 else
-{
-printf("%d ", num);
+printf("%d", num);
+
+if (num != 100)
+printf(" ");
+else
+printf("\n");
 }
 }
 printf("\n");
+
 return (0);
 }
