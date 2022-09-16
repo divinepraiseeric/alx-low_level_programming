d75d96cfcae3ad773ec761564d726f4aded9fb96
DivinePraise Eric <praisechinemerem@gmail.com>
2022-09-15 22:07:29 -0700

    t 9 b ug fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,24 +12,27 @@ int main(void)
 {
 int num;
 
-for (num = 1; num <= 100; ++num)
+for (num = 1; num <= 100; num++)
 {
-if (num % 3 == 0 && !(num % 5 == 0))
-printf("Fizz");
-else if (num % 5 == 0 && !(num % 3 == 0))
-printf("Buzz");
+if (num % 3 == 0 && (num % 5 != 0))
+{
+printf("Fizz ");
+}
+else if (num % 5 == 0 && (num % 3 != 0))
+{
+printf("Buzz ");
+}
 else if (num % 3 == 0 && num % 5 == 0)
-printf("FizzBuzz");
-else
-printf("%d", num);
-
-if (num != 100)
-printf(" ");
-else
-printf("\n");
+{
+printf("FizzBuzz ");
 }
+else
+{
+printf("%d ", num);
 }
-printf("\n");
+if (num == 100)
+printf("100\n");
+}
 
 return (0);
 }
