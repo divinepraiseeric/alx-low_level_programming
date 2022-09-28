92f5bfda4f522a6b93b39c329772e1bfaf9096aa
DivinePraise Eric <praisechinemerem@gmail.com>
2022-09-27 20:19:02 -0700

    t 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,20 +2,11 @@
 
 #define MAIN_H
 
-
-
 /**
-
 * void prototypes()
-
 * int prototypes()
-
 */
-
-
-
 int _putchar(char);
-
 char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsignedint n);
@@ -31,9 +22,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 
 void print_diagsums(int *a, int size);
-
 void set_string(char **s, char *to);
-
-
-
 #endif /*MAIN_H*/
