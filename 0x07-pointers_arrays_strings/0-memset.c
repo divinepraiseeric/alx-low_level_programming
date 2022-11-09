cbfd66c6c79ba5a775ee7aa9e70c85f7e8bdabb2
DivinePraise Eric <praisechinemerem@gmail.com>
2022-11-09 09:23:28 -0800

    Long time no C

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,18 @@
 #include "main.h"
-
-/**
-
-* _memset - fills a memory block with a constant byte
-
-* @s: address to memory block
-* @b: char to be used
-* @n: number of bytes to be used
-*
-* Return: pointer to the memory block
-
+#include <stdio.h>
+/*
+* this function fills a memory block with a constat byte of char type
+* s is a pointer referencing the memory to be filled with b
+* b is a char and will be the constant byte that fills memory
+* n not sure why I need n yet
 */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-while (n)
+for(i==0; i<n; i++)
 {
-s[n - 1] = b;
-n--;
+*(s + i) = b;
+// printf("%x and %c" (s + i), *(s + i));
 }
 return (s);
 }
