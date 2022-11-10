24e0bf734dde11cd9cac52d385cd81e37f79831d
DivinePraise Eric <praisechinemerem@gmail.com>
2022-11-10 02:49:06 -0800

    deb

---
+++ 0x07-pointers_arrays_strings/READMEE.md
@@ -0,0 +1,2 @@
+empty readme file
+meset has been written to fill a memory block with a constant character byte
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
 #ifndef MAIN_H
-
 #define MAIN_H
 
 /**
@@ -7,31 +6,18 @@
 * int prototypes()
 */
 int _putchar(char);
-
 char *_memset(char *s, char b, unsigned int n);
-
 char *s;
-
 char b;
-
 unsigned int n;
-
 int i;
-
 char f;
-
 char *_memcpy(char *dest, char *src, unsignedint n);
-
 char *_strchr(char *s, char c);
-
 unsigned int _strspn(char *s, char *accept);
-
 char *_strpbrk(char *s, char *accept);
-
 char *_strstr(char *haystack, char *needle);
-
 void print_chessboard(char (*a)[8]);
-
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 #endif /*MAIN_H*/
