a9174ca5128c7bc20fa22172e907b7bba99abe60
DivinePraise Eric <praisechinemerem@gmail.com>
2022-11-14 14:06:58 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _strchr - locates a character in a str
+* @s: address of string
+* @c: character to locate
+* Return: a pointer or NULL
+*/
+char *_strchr(char *s, char c)
+{
+	while (s++)
+	{
+		if (*s == c)
+			return (s);
+		else
+			return (NULL);
+	}
+	return (s);
+}
+
