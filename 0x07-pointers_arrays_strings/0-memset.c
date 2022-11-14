444fe8bdd56d4b73a9ac781b3ca2ea2e4a69bfe6
DivinePraise Eric <praisechinemerem@gmail.com>
2022-11-14 07:06:19 -0800

    memset should work now

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,17 @@
 #include "main.h"
-/*
-*_memset - fill a block of memory with a specific value
-*@s: starting address of memory to be filled
-*@b: the desired value
-*@n: number of bytes to be changed
-*Return: changed array with new value for n bytes
+/**
+* _memset - fill a block of memory with a specific value
+* @s: starting address of memory to be filled
+* @b: the desired value
+* @n: number of bytes to be changed
+* Return: changed array with new value for n bytes
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-char *extrapointer;
-int i = 0;
-while (n-- > 0)
-{
-*(s+i) = b;
-++i;
-}
-return s;
+	while (n-- > 0)
+	{
+		*(s + n) = b;
+	}
+	return (s);
 }
 
