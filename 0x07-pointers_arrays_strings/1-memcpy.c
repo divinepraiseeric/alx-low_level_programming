78eb51a7b4f54c8a977b2d86adbc24d54eac9154
DivinePraise Eric <praisechinemerem@gmail.com>
2022-11-14 04:51:04 -0800

    betty memcpY

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-*memcpy is meant to mimic a staple C function that copies n bytes from one memo add to another
-* src is the memory source to copy from while dest is the memory destination to copy to
+*memcpy copies n bytes from one memo add to another
+* src --- to copy from while dest --- to copy to
 * unsigned int n is the iterating range of memory area to be copied from
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	while (n-->0)
-	{	
-		*(dest+n) = *(src+n);
+	while (n-- > 0)
+	{
+		*(dest + n) = *(src + n);
 	}
-return dest;
+return (dest);
 }
 
