fc4c875119dcb96e2f536161a5756d4c5ae615ae
DivinePraise Eric <praisechinemerem@gmail.com>
2022-11-14 04:25:04 -0800

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,13 +4,12 @@
 * src is the memory source to copy from while dest is the memory destination to copy to
 * unsigned int n is the iterating range of memory area to be copied from
 */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-while (n-- > 0)
-{
-	*(dest+n) = *(src+n);
-}
+	while (n-->0)
+	{	
+		*(dest+n) = *(src+n);
+	}
 return dest;
 }
 
