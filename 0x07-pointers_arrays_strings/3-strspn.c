22da79d9207a5236d73caced080b0a53ac4c58cd
DivinePraise Eric <praisechinemerem@gmail.com>
2022-11-16 09:12:49 -0800

    strspn 1

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _strspn - gets length of prefix substring
+* @s: addr from which to calculate length of substring
+* @accept: I don't know yet
+* Return: int value
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	while (*accept++)
+	{
+		*s++ = *accept++;
+		return (s++ - s);
+	}
+}
+
