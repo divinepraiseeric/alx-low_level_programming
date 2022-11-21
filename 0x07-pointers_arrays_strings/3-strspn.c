c923060d2bcae1938e35cb4cf344e60975f62920
DivinePraise Eric <praisechinemerem@gmail.com>
2022-11-21 14:39:21 -0800

    3-strspn.c 42+

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,25 @@
 #include "main.h"
 /**
+* chkfrx - checks if a character is in a string pool
+* @pool: pointer var for the sub function
+* @x: character to find using the sub function
+* Return: int value
+*/
+unsigned int chkfrx(char *pool, char x)
+{
+	unsigned int result, p = 0;
+
+	for (; pool[p] > 0; p++)
+	{
+		if (x == pool[p])
+			result += 1;
+		else
+			result += 0;
+	}
+	return (result);
+}
+
+/**
 * _strspn - gets length of prefix substring
 * @s: addr from which to calculate length of substring
 * @accept: I don't know yet
@@ -7,14 +27,17 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0;
-	char *test;
+	unsigned int i, answer = 0;
+	unsigned int test;
 
-	for (*s = s[0]; test != 0; s++)
-	{
-		test = _strchr(accept, *s);
-		i += 1;
-	}
-	return (i - 1);
+	do {
+		test = chkfrx(accept, s[i]);
+		if (test > 0)
+		{
+			answer += 1;
+			i += 1;
+		}
+	} while (test > 0);
+	return (answer);
 }
 
