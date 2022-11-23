900611285a360ff9ca352ae3f59eae07caa45950
DivinePraise Eric <praisechinemerem@gmail.com>
2022-11-22 21:47:21 -0800

    4-strpbrk.c 1

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strpbrk - function to spot first occurence of any accept memeber in s
+ * @s: a pointer to check for spots
+ * @accept: a pointer with members to be spotted in s
+ * Return: a pointer value is returned for spots and NULL for none
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *ptr_ret;
+	unsigned int x = 0;
+	unsigned int y = 0;
+
+	for (y = 0; s[y]; y++)
+	{
+		for (x = 0; accept[x]; x++)
+		{
+			if (accept[x] == s[y])
+				ptr_ret = accept;
+		}
+	}
+	if (ptr_ret == accept)
+		return (ptr_ret);
+	else
+		return (0);
+}
+
