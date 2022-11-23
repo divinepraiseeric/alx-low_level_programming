09eb5c3b51b99bb6345cb1203215714e97a2a3a7
DivinePraise Eric <praisechinemerem@gmail.com>
2022-11-22 19:00:21 -0800

    3-strspn.c 61+

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,25 +9,24 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int answer, x, y = 0;
 	unsigned int state = 1;
-	char n[1] = "a";
 
 	for (y = 0; state != 0; y++)
 	{
-		if ((s[x] == accept[y]) && (accept[y] != n[1]))
+		if ((s[x] == accept[y]) && (accept[y] != 0))
 		{
 			answer += 1;
 			x += 1;
 			y = 0;
 		}
-		else if ((s[x] != accept[y]) && (accept[y] != n[1]))
+		else if ((s[x] != accept[y]) && (accept[y] != 0))
 		{
 			y += 0;
 		}
-		else if ((s[x] != accept[y]) && (accept[y] == n[1]))
+		else if ((s[x] != accept[y]) && (accept[y] == 0))
 		{
 			state = 0;
 		}
-		else if ((s[x] == accept[y]) && (accept[y] == n[1]))
+		else if ((s[x] == accept[y]) && (accept[y] == 0))
 		{
 			answer += 1;
 			x += 1;
