15d0517495e5f45fef9aa2ed7555c51d885faa9a
DivinePraise <divinepraiseeric@gmail.com>
2023-12-25 05:49:10 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/**
+ * main - The central function
+ *
+ * Descrption: A function that utilises printf
+ * Return: Value is 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
