4c98ddf6616c402368db21ecbfed86758ccbf7f2
DivinePraise <divinepraiseeric@gmail.com>
2023-12-25 05:20:28 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/**
+ * main - the central function a c program calls
+ *
+ * Description: A program that prints exactly
+ * "Programming is like building a multilingual puzzle"
+ * Return: A return value of 0
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
